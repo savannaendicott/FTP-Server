6f92908458dc6fc339e16b1a2f4babb15476ae6f
Kyle <kyle.thompson228@gmail.com>
2015-09-24 21:34:07 -0400

    Did a bunch of testing to connect with client

--- server.c
+++ server.c
@@ -1,5 +1,5 @@
 #include "header.h"
-
+// dem titties doe
 // TODO use errno in error messages.
 // Example:
 // 		if(send(client_socket, sendBuff, sizeof(sendBuff), 0) < 0)
@@ -13,7 +13,9 @@ int main() {
 	struct sockaddr_in serv_addr;
 
 	char recvBuff[BUFFER+1]; // +1 for the null character.
-	char command[6];
+	char *end = "&endm;\n";
+	char *end_pos = NULL;
+	int i = 0; // iterator
 
 	char request[6][6] = {"ls", "get", "put", "cd", "mkdir", "err"};
 	void (*request_handler[6]) (char* command, int client_socket) = 
@@ -41,27 +43,46 @@ int main() {
 		printf("listen error\n");
 		return 1;
 	}
-
+printf("waiting for connection\n");
 	if ((client_socket = accept(server_socket, (struct sockaddr*)NULL ,NULL)) < 0) {
 		printf("accept error\n");
 		return 1;
 	}
-  
 
 	while (1) {
+printf("waiting for message\n");
 		if(recv(client_socket, recvBuff, BUFFER, 0) < 0)
 			printf("Error: Receive\nErrno: %d\n", errno);
 		recvBuff[BUFFER] = '\n';
+printf("grabbing command out of message\n");
 
-		sscanf(recvBuff, "%s", command);
-		command[5] = '\n';
+		end_pos = strstr(recvBuff, end);
+		if (end_pos == NULL) printf("Error with end_pos\n");
+		i = end_pos - recvBuff;
+		printf("testing i: %d\n", i);
 
-		if (strcmp(command, "exit") == 0) break;
+		char command[i+1];
+		strncpy(command, recvBuff, i);
+		command[i] = '\n';
 
+printf("Command: %s\n", command);
+printf("checking for exit command\n");
+		if (strncmp(command, "exit", 4) == 0) break;
+printf("send test response\n");
+char test_msg[1025] = "testy McTesteroo\n";
+if (send(client_socket, test_msg, sizeof(test_msg), 0) < 0) printf("send error\n");
+fputs("(Client) Enter a message: ", stdout);
+fgets(test_msg, sizeof(test_msg), stdin);
+recv(client_socket, recvBuff, BUFFER, 0);
+close(client_socket);
+close(server_socket);
+return 0;
+printf("handle request\n");
 		// Handle request
 		for (int i = 0; i < 6; ++i)
 			if (strcmp(command, request[i]) == 0 || i == 5)
 				request_handler[i](recvBuff, client_socket);
+printf("request handled\n");
 	}
 
 	close(client_socket);
