1a127e543fd5287a7090569d9513efad83daeee1
Kyle <kyle.thompson228@gmail.com>
2015-09-26 17:04:41 -0400

    Continued to test around.

--- server.c
+++ server.c
@@ -15,9 +15,9 @@ int main() {
 	char recvBuff[BUFFER+1]; // +1 for the null character.
 	char *end = "&endm;\n";
 	char *end_pos = NULL;
-	int i = 0; // iterator
+	int index = 0; // iterator
 
-	char request[6][6] = {"ls", "get", "put", "cd", "mkdir", "err"};
+	char request[6][6] = {"ls   ", "get  ", "put  ", "cd   ", "mkdir", "err  "};
 	void (*request_handler[6]) (char* command, int client_socket) = 
 												{ ls, get, put, cd, mkdr, err };
 
@@ -49,39 +49,36 @@ printf("waiting for connection\n");
 		return 1;
 	}
 
+
+	int bytes_received = 0;
 	while (1) {
 printf("waiting for message\n");
-		if(recv(client_socket, recvBuff, BUFFER, 0) < 0)
-			printf("Error: Receive\nErrno: %d\n", errno);
+		while(bytes_received < 1) {
+			if((bytes_received = recv(client_socket, recvBuff, BUFFER, 0)) < 0)
+				printf("Error: Receive\nErrno: %d\n", errno);
+		}
+		bytes_received = 0;
 		recvBuff[BUFFER] = '\n';
+printf("recvBuff: '%s'\n", recvBuff);
 printf("grabbing command out of message\n");
 
 		end_pos = strstr(recvBuff, end);
 		if (end_pos == NULL) printf("Error with end_pos\n");
-		i = end_pos - recvBuff;
-		printf("testing i: %d\n", i);
+		index = end_pos - recvBuff;
 
-		char command[i+1];
-		strncpy(command, recvBuff, i);
-		command[i] = '\n';
+		char command[index+1];
+		strncpy(command, recvBuff, index);
+		command[index] = '\n';
 
 printf("Command: %s\n", command);
 printf("checking for exit command\n");
 		if (strncmp(command, "exit", 4) == 0) break;
-printf("send test response\n");
-char test_msg[1025] = "testy McTesteroo\n";
-if (send(client_socket, test_msg, sizeof(test_msg), 0) < 0) printf("send error\n");
-fputs("(Client) Enter a message: ", stdout);
-fgets(test_msg, sizeof(test_msg), stdin);
-recv(client_socket, recvBuff, BUFFER, 0);
-close(client_socket);
-close(server_socket);
-return 0;
 printf("handle request\n");
 		// Handle request
+
 		for (int i = 0; i < 6; ++i)
-			if (strcmp(command, request[i]) == 0 || i == 5)
-				request_handler[i](recvBuff, client_socket);
+			if (strncmp(command, request[i], 5) == 0 || i == 5)
+				request_handler[i](recvBuff+index+6, client_socket);
 printf("request handled\n");
 	}
 
@@ -95,7 +92,7 @@ printf("request handled\n");
 // Stretch goal: allow client to ls directories other than current
 // 		e.g. ls test/so_much_bacon/
 void ls  (char* command, int client_socket) {
-
+	printf("in ls\n");
 	// call ls and send the result to "ls.txt" and open it
 	system("ls > ls.txt");
 	int filehandle = open("ls.txt", O_RDONLY);
@@ -122,7 +119,47 @@ void ls  (char* command, int client_socket) {
 
 
 void get (char* command, int client_socket) {
+	// Get the path out of command.
+	char *end_pos = strstr(command, "&endm;\n");
+	int length = end_pos - command;
 
+	// Store the path in it's own string and open the file it points to.
+	char **path = NULL;
+	*path = (char *) malloc(length);
+	//char path[length];
+	strncpy((*path), command, length);
+	int filehandle = open(*path, O_RDONLY);
+
+	if (filehandle < 0) {
+		FILE *fp = fopen("err.txt", "w");
+		fprintf(fp, "Could not find file.");
+		fclose(fp);
+
+		filehandle = open("err.txt", O_RDONLY);
+
+		free(path);
+		*path = (char *) malloc(sizeof("err.txt"));
+		*path = "err.txt";
+	}
+
+	struct stat file_info;
+	stat(*path, &file_info);
+	int size = file_info.st_size;
+
+	// Send the size of the file and the file
+//	if (send(client_socket, &size, sizeof(int), 0) < 0) {
+//		printf("send error\n");
+//		exit(1);
+//	}
+	if (sendfile(client_socket, filehandle, NULL, size) < 0) {
+		printf("test\n");
+		printf("sendfile error\n");
+		printf("Errno: %d, %s\n", errno, strerror(errno));
+		exit(1);
+	}
+
+	// Close file_handle.
+	close(filehandle);
 }
 
 void put (char* command, int client_socket) {
@@ -142,7 +179,8 @@ void mkdr(char* command, int client_socket) {
 // Tell the client that their command was not valid.
 void err (char* command, int client_socket) {
 // create a file "err.txt" and store the error message in it.
-	FILE *fp = fopen("err.txt", "r");
+
+	FILE *fp = fopen("err.txt", "w");
 
 	char cmd[BUFFER+1];
 	sscanf(command, "%s", cmd);
@@ -156,14 +194,16 @@ void err (char* command, int client_socket) {
 	int size = file_info.st_size;
 
 // Send the client the size of the file followed by the file itself
-	if (send(client_socket, &size, sizeof(int), 0) < 0) {
+	char test[1025] = "yo holla\n";
+	if (send(client_socket, test, 1025, 0) < 0) {
 		printf("send error\n");
 		exit(1);
 	}
-	if (sendfile(client_socket, fileno(fp), NULL, size) < 0) {
-		printf("sendfile error\n");
-		exit(1);
-	}
+	recv(client_socket, test, 1025, 0);
+//	if (sendfile(client_socket, fileno(fp), NULL, size) < 0) {
+//		printf("sendfile error\n");
+//		exit(1);
+//	}
 
 // clean up err.txt
 	fclose(fp);
