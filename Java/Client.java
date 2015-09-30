4760a05e9490f67b3b6bc5600b343028a82e108b
Connor Hicks <connor@spottedu.co>
2015-09-30 16:22:04 -0400

    Added java client and updated python server

---
+++ Java/Client.java
@@ -0,0 +1,83 @@
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.OutputStream;
+import java.io.PrintStream;
+import java.net.Socket;
+import java.util.ArrayList;
+
+class Client
+{
+	public static void main(String[] args)
+	{
+		// Connect the socket
+		Socket s = null;
+		DataOutputStream dos = null;
+		DataInputStream dis = null;
+
+		try {
+			s = new Socket("127.0.0.1", 5000);
+		}
+		catch(Exception e) {
+			System.out.println("Socket Error:" + e.toString());
+		}
+		System.out.println("Connected to socket");
+
+		try {
+			// Get the input and output streams
+			dos = new DataOutputStream(s.getOutputStream());
+			dis = new DataInputStream(s.getInputStream());
+		}
+		catch (Exception e) {
+			System.out.println("dos/dis Error:" + e.toString());
+		}
+		System.out.println("Obtained dis and dos");
+
+		sendAndRecieve("ls", dos, dis);
+	    //sendAndRecieve("cd foldername");
+	    //sendAndRecieve("mkdir newFolder");
+	}
+
+	public static void sendAndRecieve(String message, DataOutputStream dos, DataInputStream dis)
+	{
+	    final String messageToSend = message;
+
+	    new Thread(new Runnable()
+	    {
+	        public void run()
+	        {
+	            try
+	            {
+					// Write to the socket
+					dos.writeUTF(message);
+					dos.flush();
+					System.out.println("Sent the message: " + message);
+
+					ArrayList<Byte> b = new ArrayList<Byte>();
+					while(dis.available() <= 0)
+					{
+						System.out.println("Waiting for response");
+					}
+
+					while(dis.available() > 0)
+					{
+						b.add(dis.readByte());
+					}
+					
+					System.out.println(b.toString());
+
+					dis.close();
+					dos.close();
+	            }
+	            catch(Exception e)
+	            {
+	                System.out.println("Error:" + e.toString());
+	            }
+	        }
+	    }).start();
+	}
+}
--- Python/server.py
+++ Python/server.py
@@ -2,24 +2,28 @@ import socket
 import subprocess
 import os
 
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)         
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.bind(('', 5000))
 
-s.listen(5)           
-c, addr = s.accept()    
+s.listen(5)
+print("Listening on port 5000")
+c, addr = s.accept()
 
-while True:
-    message = c.recv(1024)
+def recieve_reply():
+    message = str(c.recv(1024))
     message_parts = message.split()
-    command = message_parts[0].strip()
+    command = message_parts[0].strip()[2:]
+
+    print len(command)
+
+    print(command == "ls")
     if len(message_parts) > 1:
         parameter = message_parts[1]
-
-
 
     if command == "ls":
         proc = subprocess.Popen(["ls"], stdout=subprocess.PIPE, shell=True)
         (out, err) = proc.communicate()
+        print(out)
         c.send(out)
 
 
@@ -36,31 +40,33 @@ while True:
     elif command == "get":
         size = os.path.getsize(parameter)
         c.send(str(size))
-        
+
         with open(parameter,'rb') as f:
             c.sendall(f.read())
 
 
     elif command == "put":
         chunks = []
-        
+
         while "&endm;" not in chunks:
             chunk = c.recv(4096)
             chunks += chunk
             if "&endm;" in chunk:
                 break
-            
+
         msg = chunks[:-6]
-        
+
         with open(parameter,'wb') as f:
             f.write(b''.join(msg))
         print "end put"
-            
+
 
     elif command == "exit":
         s.close()
-        break
 
 
     else:
         c.send("command: '%s' not recognized" % command)
+
+
+recieve_reply()
